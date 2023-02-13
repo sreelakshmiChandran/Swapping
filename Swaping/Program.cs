25a407975a3329607e6502935c81fc0394cb6b61
Sreelakshmi Chandran <sreelakshmi.chandran@experionglobal.local>
2023-02-13 15:23:55 +0530

    first commit

---
+++ Swaping/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Swaping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the 1st number a =");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the 1st number b =");
+            int b = int.Parse(Console.ReadLine());
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+        }
+    }
+}
